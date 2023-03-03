a2992c12afadab1569a8c6a3541305479de679a3
Magnus Ersdal <magnus@resani.com>
2023-03-03 17:56:33 +0100

    documentation updates and packaging polish

--- setup.py
+++ setup.py
@@ -2,13 +2,23 @@ from setuptools import setup
 
 setup(
     name='kicad_gitdiff',
-    version='0.0.1',
-    url='',
+    version='0.0.3',
+    url='https://github.com/maersdal/kicad_gitdiff_cli',
     license='Apache-2.0',
     license_files=["LICENSE"],
     author='magnus ersdal',
     author_email='magnus@resani.com',
-    description='',
+    description='A tool for a visual diff with git and kicad',
+    long_description="""
+Simple visual diff for kicad 7 or higher. one can easily extend this tool to build a diff-er for anything that outputs svg.
+
+This is not an official kicad tool or anything, just a friday night project.
+
+My goal was just to get some `git` integration with the Kicad file formats.
+
+[Documentation on github](https://github.com/maersdal/kicad_gitdiff_cli#readme)
+""",
+    long_description_content_type="text/markdown",
     package_dir={"": "src"},
     packages=['git_config_template', 'kicad_export', 'sch_diff', 'svg_diff', 'checkers'],
     package_data={"git_config_template": ["*_template"]},
